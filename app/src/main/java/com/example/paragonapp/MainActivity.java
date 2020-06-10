a0507001bc4c4fa3c423a0b27552af7a85766f20
naters123 <naters@byui.edu>
2020-06-09 18:23:01 -0600

    Guest button now takes you to the selection screen

--- app/src/main/java/com/example/paragonapp/MainActivity.java
+++ app/src/main/java/com/example/paragonapp/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.paragonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button btnGuest;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGuest = (Button)findViewById(R.id.guestbtn);
+
+        btnGuest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent create = new Intent(MainActivity.this, loggedIn.class);
+                startActivity(create);
+            }
+        });
     }
 }
-//testpush1
+
---
+++ app/src/main/java/com/example/paragonapp/loggedIn.java
@@ -0,0 +1,14 @@
+package com.example.paragonapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loggedIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logged_in);
+    }
+}
