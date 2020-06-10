5b971903ce70584bce90c649ad2eba64cc48b238
sth17001 <for3st@byui.edu>
2020-06-10 17:55:18 -0600

    connected login button to login activity

--- app/src/main/java/com/example/paragonapp/MainActivity.java
+++ app/src/main/java/com/example/paragonapp/MainActivity.java
@@ -9,17 +9,26 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 Button btnGuest;
+Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnGuest = (Button)findViewById(R.id.guestbtn);
+        btnLogin = (Button)findViewById(R.id.loginbtn);
 
         btnGuest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent create = new Intent(MainActivity.this, loggedIn.class);
+                startActivity(create);
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent create = new Intent(MainActivity.this, login.class);
                 startActivity(create);
             }
         });
