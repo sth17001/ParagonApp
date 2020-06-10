3531a0c1364edc56ee592583cf0ff86ea8410b65
naters123 <naters@byui.edu>
2020-06-09 19:35:14 -0600

    Added an OrderOnline activity that gives us a bug

--- app/src/main/java/com/example/paragonapp/loggedIn.java
+++ app/src/main/java/com/example/paragonapp/loggedIn.java
@@ -2,13 +2,29 @@ package com.example.paragonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class loggedIn extends AppCompatActivity {
-
+ImageButton btnOrderOnline;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+
+        btnOrderOnline = (ImageButton)findViewById(R.id.orderOnline);
+
+        btnOrderOnline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent create = new Intent(loggedIn.this, OrderOnline.class);
+                startActivity(create);
+            }
+        });
+
+
     }
 }
