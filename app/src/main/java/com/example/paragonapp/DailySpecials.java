05623d82afa78058e096922c6be6245eef384c07
shoren <shoren@byui.edu>
2020-06-10 17:23:50 -0600

    Added to the Daily Specials!

--- app/src/main/java/com/example/paragonapp/DailySpecials.java
+++ app/src/main/java/com/example/paragonapp/DailySpecials.java
@@ -2,13 +2,41 @@ package com.example.paragonapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DailySpecials extends AppCompatActivity {
-
+Button dailyButton,weeklyButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_specials);
+
+        dailyButton = (Button)findViewById(R.id.dailyButton);
+        weeklyButton = (Button)findViewById(R.id.weeklyButton);
+
+        dailyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Code here executes on main thread after user presses button
+            }
+        });
+        weeklyButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Code here executes on main thread after user presses button
+            }
+        });
+
+
+
+
+
+
+
+
+
     }
 }
