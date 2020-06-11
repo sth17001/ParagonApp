7099428b9a60c2756ea2131155dede150d70680f
shoren <shoren@byui.edu>
2020-06-10 18:18:26 -0600

    Added functionality to the buttons on the daily special page. Need to research SubScale View to be able to zoom in on the image.

--- app/src/main/java/com/example/paragonapp/DailySpecials.java
+++ app/src/main/java/com/example/paragonapp/DailySpecials.java
@@ -6,26 +6,37 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class DailySpecials extends AppCompatActivity {
 Button dailyButton,weeklyButton;
+ImageView daily,paragonweekly;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_specials);
 
+        //Buttons for Switching between daily and weekly specials
         dailyButton = (Button)findViewById(R.id.dailyButton);
         weeklyButton = (Button)findViewById(R.id.weeklyButton);
 
+        // Images for daily and weekly specials
+        daily = (ImageView)findViewById(R.id.dailyPic);
+        paragonweekly = (ImageView)findViewById(R.id.weeklyPic);
+
         dailyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+            daily.setVisibility(View.GONE);
+            paragonweekly.setVisibility(View.VISIBLE);
                 // Code here executes on main thread after user presses button
             }
         });
         weeklyButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                daily.setVisibility(View.VISIBLE);
+                paragonweekly.setVisibility(View.GONE);
                 // Code here executes on main thread after user presses button
             }
         });
