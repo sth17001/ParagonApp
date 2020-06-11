0515e4deddbf279997878e0d28d2f61c0bd72bc2
sth17001 <for3st@byui.edu>
2020-06-10 18:10:54 -0600

    firebase changes

--- app/src/main/java/com/example/paragonapp/login.java
+++ app/src/main/java/com/example/paragonapp/login.java
@@ -1,24 +1,47 @@
 package com.example.paragonapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class login extends AppCompatActivity {
-
+    EditText getUsername, getPassword;
+    String emailOrUsername, password;
+    DatabaseReference table_user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        table_user = database.getReference("User");
     }
 
     public void logIn(View view) {
-        EditText getUsername = (EditText) findViewById(R.id.editText1);
-        EditText getPassword = (EditText) findViewById(R.id.editText2);
-        String emailOrUsername = getUsername.getText().toString();
-        String password = getPassword.getText().toString();
+        getUsername = (EditText) findViewById(R.id.editText1);
+        getPassword = (EditText) findViewById(R.id.editText2);
+        emailOrUsername = getUsername.getText().toString();
+        password = getPassword.getText().toString();
+        table_user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        })
 
     }
 }
