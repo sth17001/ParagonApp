7300c906fe9b886cccb267f26e1db661dca97277
naters123 <naters@byui.edu>
2020-06-10 18:04:58 -0600

    added firebase implementation thing

--- app/src/main/java/com/example/paragonapp/OrderOnline.java
+++ app/src/main/java/com/example/paragonapp/OrderOnline.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
@@ -17,6 +18,7 @@ public class OrderOnline extends AppCompatActivity {
     ListView cartItemsL;
     LinearLayout checkoutLayout, menuLayout, cartLayout;
     Button menuBtn, cartBtn, checkoutBtn;
+    ImageButton grilledIbtn, friedIbtn, sandwichIbtn, specialIbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,7 @@ public class OrderOnline extends AppCompatActivity {
         setContentView(R.layout.activity_order_online);
 
         cartItemsL = (ListView)findViewById(R.id.cartItems);
+
         checkoutLayout = (LinearLayout) findViewById(R.id.checkout);
         menuLayout = (LinearLayout)findViewById(R.id.menu);
         cartLayout = (LinearLayout)findViewById(R.id.cart);
@@ -31,6 +34,11 @@ public class OrderOnline extends AppCompatActivity {
         cartBtn = (Button) findViewById(R.id.cartBTN);
         menuBtn = (Button) findViewById(R.id.menuBTN);
         checkoutBtn = (Button) findViewById(R.id.checkoutBTN);
+
+        grilledIbtn = (ImageButton) findViewById(R.id.grilledBTN);
+        friedIbtn = (ImageButton) findViewById(R.id.friedBTN);
+        sandwichIbtn = (ImageButton) findViewById(R.id.sandwichBTN);
+        specialIbtn = (ImageButton) findViewById(R.id.specialBTN);
 
 
         for (Integer i = 0; i < 10; i++) {
@@ -56,6 +64,10 @@ public class OrderOnline extends AppCompatActivity {
                 cartLayout.setVisibility(View.GONE);
                 menuLayout.setVisibility(View.VISIBLE);
                 checkoutLayout.setVisibility(View.GONE);
+                sandwichIbtn.setVisibility(View.VISIBLE);
+                friedIbtn.setVisibility(View.VISIBLE);
+                grilledIbtn.setVisibility(View.VISIBLE);
+                specialIbtn.setVisibility(View.VISIBLE);
 
             }
         });
@@ -66,6 +78,39 @@ public class OrderOnline extends AppCompatActivity {
                 cartLayout.setVisibility(View.GONE);
                 menuLayout.setVisibility(View.GONE);
                 checkoutLayout.setVisibility(View.VISIBLE);
+            }
+        });
+
+        specialIbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sandwichIbtn.setVisibility(View.GONE);
+                friedIbtn.setVisibility(View.GONE);
+                grilledIbtn.setVisibility(View.GONE);
+            }
+        });
+        sandwichIbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                specialIbtn.setVisibility(View.GONE);
+                friedIbtn.setVisibility(View.GONE);
+                grilledIbtn.setVisibility(View.GONE);
+            }
+        });
+        friedIbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sandwichIbtn.setVisibility(View.GONE);
+                specialIbtn.setVisibility(View.GONE);
+                grilledIbtn.setVisibility(View.GONE);
+            }
+        });
+        grilledIbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sandwichIbtn.setVisibility(View.GONE);
+                friedIbtn.setVisibility(View.GONE);
+                specialIbtn.setVisibility(View.GONE);
             }
         });
     }
