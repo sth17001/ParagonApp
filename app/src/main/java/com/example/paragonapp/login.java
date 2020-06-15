05005e51141a5185e8703d4ffd13aa7a625b8cd6
naters123 <naters@byui.edu>
2020-06-15 15:14:28 -0600

    added firebase implementation thing

--- app/src/main/java/com/example/paragonapp/login.java
+++ app/src/main/java/com/example/paragonapp/login.java
@@ -11,6 +11,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -21,40 +22,51 @@ public class login extends AppCompatActivity {
     String emailOrUsername, password;
     DatabaseReference table_user;
     boolean loggedin;
+    Button loginBtn;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        loginBtn = (Button)findViewById(R.id.loginBTN);
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         table_user = database.getReference("User");
-    }
 
-    public void logIn(View view) {
-        getUsername = (EditText) findViewById(R.id.usernameEdit);
-        getPassword = (EditText) findViewById(R.id.passwordEdit);
-        emailOrUsername = getUsername.getText().toString();
-        password = getPassword.getText().toString();
-        table_user.addValueEventListener(new ValueEventListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                if (dataSnapshot.child(getUsername.getText().toString()).exists()) {
-                    User user = dataSnapshot.child(getUsername.getText().toString()).getValue(User.class);
-                    if (user.getPassword().equals(password)) {
-                        loggedin = true;
-                        Toast.makeText(login.this, "Logged in", LENGTH_LONG).show();
-                    } else {
-                        Toast.makeText(login.this, "Login failed", LENGTH_LONG).show();
+            public void onClick(View v) {
+                getUsername = (EditText) findViewById(R.id.usernameEdit);
+                getPassword = (EditText) findViewById(R.id.passwordEdit);
+                emailOrUsername = getUsername.getText().toString();
+                password = getPassword.getText().toString();
+                System.out.println(password+ " ========================================================================");
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.child(getUsername.getText().toString()).exists()) {
+                            User user = dataSnapshot.child.getValue(User.class);
+                            System.out.println(user.getPassword()+ " ========================================================================");
+                            /*if (user.getPassword().equals(password)) {
+                                loggedin = true;
+                                Toast.makeText(login.this, "Logged in", LENGTH_LONG).show();
+                            }
+                            else {
+                                Toast.makeText(login.this, "Login failed", LENGTH_LONG).show();
+                            }*/
+                        }
                     }
-                }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
             }
         });
-
     }
+
 }
