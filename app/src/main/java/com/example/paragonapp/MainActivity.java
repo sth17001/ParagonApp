70a6ca79387906c729d4242c681e84953b4b9cb4
naters123 <naters@byui.edu>
2020-06-16 03:50:22 -0600

    login screen XML changes

--- app/src/main/java/com/example/paragonapp/MainActivity.java
+++ app/src/main/java/com/example/paragonapp/MainActivity.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 2000;
+    private static int SPLASH_TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
