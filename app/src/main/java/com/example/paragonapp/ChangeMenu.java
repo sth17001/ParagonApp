3ba34b0542d104e38cfc1604d08ebdbd093f7b7c
sth17001 <for3st@byui.edu>
2020-06-27 23:35:52 -0600

    manager options update

--- app/src/main/java/com/example/paragonapp/ChangeMenu.java
+++ app/src/main/java/com/example/paragonapp/ChangeMenu.java
@@ -13,8 +13,10 @@ import com.google.firebase.database.ValueEventListener;
 import java.util.ArrayList;
 
 public class ChangeMenu extends AppCompatActivity {
-    DatabaseReference friedItemsRef;// grilledItemsRef, specialItemsRef;
+    DatabaseReference friedItemsRef, grilledItemsRef, specialItemsRef;
     ArrayList<Item> friedArrayList = new ArrayList<Item>();
+    ArrayList<Item> grilledArrayList = new ArrayList<Item>();
+    ArrayList<Item> specialArrayList = new ArrayList<Item>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,19 +24,18 @@ public class ChangeMenu extends AppCompatActivity {
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         friedItemsRef = database.getReference("FriedItems");
-        //grilledItemsRef = database.getReference("GrilledItems");
-        //specialItemsRef = database.getReference("SpecialItems");
+        grilledItemsRef = database.getReference("GrilledItems");
+        specialItemsRef = database.getReference("SpecialItems");
 
         downloadItems();
 
-        //System.out.println("array size: " + friedArrayList.size());
-        displaySize();
     }
 
     public void downloadItems() {
         friedItemsRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
                 for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
                     String name = userSnap.child("name").getValue().toString();
                     String price = userSnap.child("price").getValue().toString();
@@ -42,7 +43,8 @@ public class ChangeMenu extends AppCompatActivity {
                     Item item = new Item(name, price);
 
                     friedArrayList.add(item);
-                    System.out.println("Downloading... " + friedArrayList.size());
+                    System.out.println("Downloading... " + friedArrayList.get(i).getName());
+                    i++;
                 }
                 System.out.println(friedArrayList.size() + " items downloaded.");
             }
@@ -53,8 +55,53 @@ public class ChangeMenu extends AppCompatActivity {
                 System.out.println("Failed to read value.");
             }
         });
-    }
-    public void displaySize() {
-        System.out.println("array size: " + friedArrayList.size());
+
+        grilledItemsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+                for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
+                    String name = userSnap.child("name").getValue().toString();
+                    String price = userSnap.child("price").getValue().toString();
+
+                    Item item = new Item(name, price);
+
+                    grilledArrayList.add(item);
+                    System.out.println("Downloading... " + grilledArrayList.get(i).getName());
+                    i++;
+                }
+                System.out.println(grilledArrayList.size() + " items downloaded.");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value.");
+            }
+        });
+
+        specialItemsRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                int i = 0;
+                for (DataSnapshot userSnap : dataSnapshot.getChildren()) {
+                    String name = userSnap.child("name").getValue().toString();
+                    String price = userSnap.child("price").getValue().toString();
+
+                    Item item = new Item(name, price);
+
+                    specialArrayList.add(item);
+                    System.out.println("Downloading... " + specialArrayList.get(i).getName());
+                    i++;
+                }
+                System.out.println(specialArrayList.size() + " items downloaded.");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                System.out.println("Failed to read value.");
+            }
+        });
     }
 }
